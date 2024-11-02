c6876d79445f8d79ee86889b9c2062e3d234697c
Nakul Suryan <iamnakulsuryan@gmail.com>
2024-11-02 21:37:12 +0530

    cloudbuild try 22

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Nakul bro!';
+    return 'Hello Nakul bro - with node dist/main!';
   }
 }
